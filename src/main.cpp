df40cf55d4e75c3bf3f895c4e280044e805da473
Kosyo Nikolov <kosyo.nikolov@protonmail.com>
2021-10-26 20:23:11 +0300

    double intermediate stage

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,13 @@ void source(Pipe<int> & writeHead)
 
 void stage1(Pipe<int> & readHead, Pipe<double> & writeHead)
 {
+    static int cnt = 0;
+    static std::mutex m;
+
+    m.lock();
+    cnt++;
+    m.unlock();
+
     int inVal;
     while (readHead.read(inVal))
     {
@@ -28,7 +35,11 @@ void stage1(Pipe<int> & readHead, Pipe<double> & writeHead)
         // std::cout << "pass " << val << "\n";
         writeHead.write(std::sqrt(val));
     }
-    writeHead.closeWrite();
+
+    m.lock();
+    cnt--;
+    if (cnt <= 0) writeHead.closeWrite();
+    m.unlock();
 }
 
 void sink(Pipe<double> & readHead)
@@ -37,7 +48,7 @@ void sink(Pipe<double> & readHead)
     double inVal;
     while (readHead.read(inVal))
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
         std::cout << inVal << "\n";
         sum += inVal;
     }
@@ -50,13 +61,22 @@ int main(int, char**)
     Pipe<int> pipe1(10);
     Pipe<double> pipe2(10);
 
+    auto tStart = std::chrono::steady_clock::now();
+
     std::thread tSrc([&]() { source(pipe1); });
-    std::thread tStage1([&]() { stage1(pipe1, pipe2);});
+    std::thread tStage1_1([&]() { stage1(pipe1, pipe2);});
+    std::thread tStage1_2([&]() { stage1(pipe1, pipe2);});
     std::thread tSink([&]() { sink(pipe2); });
 
     tSrc.join();
-    tStage1.join();
+    tStage1_1.join();
+    tStage1_2.join();
     tSink.join();
+
+    auto tEnd = std::chrono::steady_clock::now();
+    const auto tMs = std::chrono::duration_cast<std::chrono::milliseconds>(tEnd - tStart).count();
+
+    std::cout << "Elapsed time: " << tMs << " ms\n";
 
     return 0;
 }
