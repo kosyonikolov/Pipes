11c50ffd45793322df59266728223bd218d701b0
Kosyo Nikolov <kosyo.nikolov@protonmail.com>
2022-02-05 10:25:26 +0200

    add qpipe concept

--- MtLib/inc/MtLib/cpipe.h
+++ MtLib/inc/MtLib/cpipe.h
@@ -21,6 +21,25 @@ namespace mt
 
     template<typename P, typename T>
     concept pipe = read_pipe<P, T> && write_pipe<P, T>;
+
+    template<typename P, typename T>
+    concept read_qpipe = requires(P pipe, T val, const T * ptr_val)
+    {
+        {pipe.begin_read()} -> std::same_as<const T*>;
+        {pipe.finish_read(ptr_val)};
+        {pipe.close_read()};
+    };
+
+    template<typename P, typename T>
+    concept write_qpipe = requires(P pipe, T val, T * ptr_val)
+    {
+        {pipe.begin_write()} -> std::same_as<T*>;
+        {pipe.finish_write(ptr_val)};
+        {pipe.close_write()};
+    };
+
+    template<typename P, typename T>
+    concept qpipe = read_qpipe<P, T> && write_qpipe<P, T>;
 }
 
 #endif
